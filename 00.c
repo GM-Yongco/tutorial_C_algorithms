ebc339fc8e5a93356cc9db0ba33b6ed50ef42522
Giulio Yongco <130813239+GM-Yongco@users.noreply.github.com>
2024-08-19 22:19:55 +0800

    initial commit

---
+++ 00.c
@@ -0,0 +1,21 @@
+// =======================================================================
+// Author          : G.M. Yongco #BeSomeoneWhoCanStandByShinomiya
+// Date            : ur my date uwu
+// Description     : Code that will impress u ;)
+// =======================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "01.h"
+
+// =======================================================================
+// MAIN
+// =======================================================================
+
+int main(){
+	separator("START");
+	
+	separator("END");
+	return 0;
+}
---
+++ 01.h
@@ -0,0 +1,40 @@
+// =======================================================================
+// Author          : G.M. Yongco #BeSomeoneWhoCanStandByShinomiya
+// Date            : ur my date uwu
+// rules			:
+//		use snake case
+//		all user defined typedefs should be all caps
+// Description     : Code that will impress u ;)
+// =======================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum{False, True}BOOLEAN;
+
+// =======================================================================
+// MISC FUNCTIONS
+// =======================================================================
+
+void separator(char* message){
+	int count = 55; 				//additional 5 for the (\n)s and the \0
+	char new_string[count];
+
+	memset(new_string, '-', count);
+
+	new_string[0] = '\n';
+	new_string[1] = '\n';
+	new_string[count-2] = '\n';
+	new_string[count-3] = '\n';
+	new_string[count-1] = '\0';
+
+	memcpy(new_string + 2, message, strlen(message));
+	new_string[strlen(message) + 2] = ' ';
+
+	printf("%s", new_string);
+}
+
+// =======================================================================
+// CORE FUNCTIONS 1
+// =======================================================================
